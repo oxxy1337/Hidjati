02c09943313f9931b03809640b0af29a6f1ac8e6
Dermi Malika <48280218+Malika3@users.noreply.github.com>
2020-05-11 22:49:13 -1100

    Auth Forms in web site

--- Web-site/test/server.js
+++ Web-site/test/server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = 80;
+var port = 3000;
+
 
 app.use(express.static(__dirname + '/public'));
 app.use('/css',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')));
@@ -10,8 +11,22 @@ app.use('/js',express.static(path.join(__dirname,'/node_modules/bootstrap/jquery
 
 app.set('views','./src/views');
 app.set('view engine','ejs');
+
+
 app.get('/', function(req, res) {
     res.render('index');
+});
+app.get('/Go',function(req2,res2){
+    res2.render('GuideOmra');
+});
+app.get('/Gh',function(req3,res3){
+    res3.render('GuideHadj');
+});
+app.get('/Login',function(req4,res4){
+    res4.render('Login');
+});
+app.get('/SignUp',function(req5,res5){
+    res5.render('SignUp');
 });
 
 app.listen(port);
