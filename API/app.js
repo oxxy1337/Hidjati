2b5e185396eceda2f836bf738ca5ed863ec92f7b
BaK1ri <a.bakiri@esi-sba.dz>
2020-07-05 21:54:06 +0100

    add mongodb cluster

--- API/app.js
+++ API/app.js
@@ -56,7 +56,7 @@ app.post('/test', upload.single('name'), (req, res, next)=>{
 
 // Add headers
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
